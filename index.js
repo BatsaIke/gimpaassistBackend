cacc21fcd865eba02441510de173863b6474af44
IsaacKimo <139249129+IsaacKimo@users.noreply.github.com>
2023-11-12 21:00:31 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const OpenAI = require('openai');
+const fs = require('fs');
+require('dotenv').config();
+
+const app = express(); 
+
+// Set your OpenAI API key
+const apiKey = process.env.OPENAI_API_KEY;
+
+if (!apiKey) {
+  console.error("OpenAI API key is missing. Please set the OPENAI_API_KEY environment variable.");
+  process.exit(1);
+}
+
+const openai = new OpenAI({ apiKey: apiKey });
+
+// Route for handling POST requests
+app.use(express.json()); // Parse JSON requests
+app.post('/response', async (req, res) => {
+  try {
+    // Extract the prompt from the request body
+    const prompt = req.body.prompt;
+
+    if (!prompt) {
+      return res.status(400).json({ error: 'Enter a prompt to get a feedback.' });
+    }
+
+    const promptText = `${prompt}`;
+
+    const completion = await openai.chat.completions.create({
+      model: 'text-davinci-003', // Provide the model/engine name
+      prompt: promptText,
+      max_tokens: 100, // Adjust as needed
+    });
+
+    const responseText = completion.choices[0].text;
+    res.json({ response: responseText });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Start the server
+const port = 6000;
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
---
+++ savecode.js
@@ -0,0 +1,49 @@
+
+const OpenAI = require("openai");
+
+// Set your OpenAI API key
+const apiKey = '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';
+
+const openai = new OpenAI({ apiKey });
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{"role": "system", "content": "You are a helpful assistant."},
+        {"role": "user", "content": "Who won the world series in 2020?"},
+        {"role": "assistant", "content": "The Los Angeles Dodgers won the World Series in 2020."},
+        {"role": "user", "content": "Where was it played?"}],
+    model: "gpt-3.5-turbo",
+  });
+
+  console.log(completion.choices[0]);
+}
+main();
+
+
+// const openai = require('openai');
+// const fs = require('fs');
+
+// // Set your OpenAI API key
+// const apiKey = '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';
+// const prompt = fs.readFileSync('data.txt', 'utf-8'); // Assuming your data is in a file named data.txt
+
+// const openaiInstance = new openai.OpenAI({apiKey:"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"});
+// console.log("HERE IS THE INSANCE")
+
+// // Example prompt
+// const promptText = `
+//   Your prompt goes here: ${prompt}
+// `;
+
+// // Make a request to OpenAI
+// openaiInstance.complete({
+//   engine: 'text-davinci-003', // You can use a different engine if needed
+//   prompt: promptText,
+//   max_tokens: 100, // Adjust as needed
+// })
+//   .then(response => {
+//     console.log(response.choices[0].text);
+//   })
+//   .catch(error => {
+//     console.error(error);
+//   });
